c5c10444cf69d17fb9407d1fb00803f7c89b5856
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-21 21:07:40 +0400

    Parsing The Incoming Request & Executing Server-side Code

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
-    res.status(200).json({
-        message: 'This works!'
-    })
+    if(req.method === 'POST'){
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText,
+        };
+
+        // store that in a database or in a file
+        const filepath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filepath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filepath, JSON.stringify(data));
+        res.status(201).json({message: 'Success!', feedback: newFeedback})
+    }else{
+        res.status(200).json({ message: 'This works!' })
+    }
+
 }
 
 export default handler
