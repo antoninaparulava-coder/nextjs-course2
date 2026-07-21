f454f3b3bdd4ac48bf2b255e5500612af94310e8
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-21 22:02:14 +0400

    Using API Routes To Get Data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from 'react';
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
@@ -10,17 +12,25 @@ function HomePage() {
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
 
-    const reqBody = { email: enteredEmail, text: enteredFeedback}
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
 
-    fetch('/api/feedback',{
+    fetch('/api/feedback', {
       method: 'POST',
       body: JSON.stringify(reqBody),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
   }
 
   return (
@@ -28,15 +38,22 @@ function HomePage() {
       <h1>The Home Page</h1>
       <form onSubmit={submitFormHandler}>
         <div>
-          <label htmlFor="email" >Your Email Anddress</label>
-          <input type="email" id="email" ref={emailInputRef} />
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInputRef} />
         </div>
         <div>
-          <label htmlFor="feedback" >Your Feedback</label>
-          <textarea id="feedback" rows='5' ref={feedbackInputRef}></textarea>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
